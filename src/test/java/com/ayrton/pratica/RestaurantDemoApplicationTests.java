6fd307e1c7cd7a9c0b37a7dd98d0209f4a5be609
ec-ayrton <ec.ayrton@gmail.com>
2020-09-06 13:23:29 -0300

    projeto criado

---
+++ src/test/java/com/ayrton/pratica/RestaurantDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ayrton.pratica;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
